9649fc4708e0dd686a71f5e4476684613f9b1676
Michael Liao <liaomichael3@gmail.com>
2017-12-03 00:16:55 -0600

    bro

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -2,7 +2,7 @@
 $connect = mysql_connect("localhost", "root", "password123", "crimedata");
 /* check connection */
 if (mysql_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
+    printf("Connect failed: %s\n", mysql_connect_error());
     exit();
 }
   $qry =  mysql_query("create procedure neighborsearch() SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='Bridgeport'");
