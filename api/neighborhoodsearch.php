4136f54d1da46a60f9b1c6fb29ead82bab1dd819
Michael Liao <liaomichael3@gmail.com>
2017-12-03 00:08:35 -0600

    bro

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno()) {
 }
   $querySubTbl =  mysql_query("CREATE PROCEDURE neighborsearch() BEGIN SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='Bridgeport'");
 
-  $resultSubTbl = mysql_query("call user()");
+  $resultSubTbl = mysql_query("call neighborhoodsearch()");
 	if(mysql_num_rows($resultSubTbl)>0){
 		while($row = mysql_fetch_array($resultSubTbl))
 		{
