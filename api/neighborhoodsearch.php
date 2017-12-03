af75bde44e9cc81b0d6443c1694de08404b4f7ff
Michael Liao <liaomichael3@gmail.com>
2017-12-03 00:44:09 -0600

    bro

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -6,9 +6,6 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
-  $data = json_decode(file_get_contents("php://input"));
-  $keyword = mysqli_real_escape_string($connect, $data->keyword);
-  $output = array();
   $qry =  mysqli_query($connect, "create procedure neighborhoodsearch() SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='Lincoln Park'");;
   echo "Stored Procedure created.";
   $res = mysqli_query($connect,"call neighborhoodsearch()");
