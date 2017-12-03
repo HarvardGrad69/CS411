e3ed6f272ad0469aa9c0063912652e4d7112db68
Michael Liao <liaomichael3@gmail.com>
2017-12-03 00:16:17 -0600

    bro

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -1,11 +1,11 @@
 <?php
 $connect = mysql_connect("localhost", "root", "password123", "crimedata");
 /* check connection */
-if (mysqli_connect_errno()) {
+if (mysql_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-  $qry =  mysql_query("CREATE PROCEDURE neighborsearch() BEGIN SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='Bridgeport'");
+  $qry =  mysql_query("create procedure neighborsearch() SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='Bridgeport'");
   echo "Stored Procedure created.";
   mysql_query($qry,$connect);
 
