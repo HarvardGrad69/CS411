c74af2ea1cedc92771090f871a970c9b1a1da0bf
Michael Liao <liaomichael3@gmail.com>
2017-12-03 00:51:19 -0600

    bro

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -6,8 +6,8 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
-  $data = json_decode(file_get_contents("php://input"));
-  $keyword = mysqli_real_escape_string($connect, $data->keyword);
+//  $data = json_decode(file_get_contents("php://input"));
+//  $keyword = mysqli_real_escape_string($connect, $data->keyword);
   $output = array();
   $qry =  mysqli_query($connect, "create procedure search() SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
   $res = mysqli_query($connect,"call search()");
