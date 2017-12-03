eb282eb061445bad449119e0cb8c5b8d542f80b7
HarvardGrad69 <33505257+HarvardGrad69@users.noreply.github.com>
2017-12-02 23:04:33 -0600

    Delete neighborsearch.php

--- neighborsearch.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$result = mysqli_query('CREATE PROCEDURE `neighborsearch`(IN $keyword) BEGIN SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood=$keyword; END');
-
-?>
