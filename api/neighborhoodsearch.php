e097f96c926f59f68c4bed17561a31b2b5ad5861
Michael Liao <liaomichael3@gmail.com>
2017-12-03 01:30:29 -0600

    bro

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -12,9 +12,9 @@ if (mysqli_connect_errno()) {
   //echo $keyword;
   //$output = array();
   include 'filterCrimeData.php';
-  //$qry =  mysqli_query($connect, "create procedure updoot() SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
-  $stmt = $connection->prepare("SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
-  $stmt->bind_param("sss", $keyword);
+  $qry =  mysqli_query($connect, "create procedure (IN $keyword) updoot() SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
+  // $stmt = $connection->prepare("SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
+  // $stmt->bind_param("sss", $keyword);
   //$stmt->execute();
   //$res = mysqli_query($connect,"call doot()");
 
