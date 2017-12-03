15a1d76f389648bb50854beb9d4cf417134891b9
Michael Liao <liaomichael3@gmail.com>
2017-12-03 01:19:08 -0600

    bro

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -11,13 +11,13 @@ if (mysqli_connect_errno()) {
   // $keyword = $_SESSION['keyword'];
   echo $keyword;
   $output = array();
-  $qry =  mysqli_query($connect, "create procedure search() SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='Armour Square'");
-  $res = mysqli_query($connect,"call search()");
-
-  while ($row = mysqli_fetch_array($res)){
-    $output[] = $row;
-  }
-  echo json_encode($output);
+  $qry =  mysqli_query($connect, "create procedure doot() SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='Armour Square'");
+  // $res = mysqli_query($connect,"call doot()");
+  //
+  // while ($row = mysqli_fetch_array($res)){
+  //   $output[] = $row;
+  // }
+  // echo json_encode($output);
 
 
 ?>
