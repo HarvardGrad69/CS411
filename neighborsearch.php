448e45f219df27f32b4e0c23a20103b0195061fa
Michael Liao <liaomichael3@gmail.com>
2017-12-02 22:46:23 -0600

    no

--- neighborsearch.php
+++ neighborsearch.php
@@ -1,5 +1,5 @@
 <?php
 
-$result = mysqli_query('CREATE PROCEDURE `neighborsearch`(IN $keyword) BEGIN SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'; END');
+$result = mysqli_query('CREATE PROCEDURE `neighborsearch`(IN $keyword) BEGIN SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood=$keyword; END');
 
 ?>
