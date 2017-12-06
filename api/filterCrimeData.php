c908a1f70106a40bd1bbea8ba9443baba2757f89
Michael Liao <liaomichael3@gmail.com>
2017-12-05 21:36:22 -0600

    o

--- api/filterCrimeData.php
+++ api/filterCrimeData.php
@@ -13,8 +13,8 @@ if (mysqli_connect_errno()) {
 	$output = array();
 	$querySubTbl =
 		"SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'";
-	// $resultSubTbl = mysqli_query($connect, "call dit($keyword)");
-  $resultSubTbl = CALL doaa($keyword);
+	$resultSubTbl = mysqli_query($connect, "CALL dit($keyword") or die("Query fail: " . mysqli_error());;
+  // $resultSubTbl = CALL doaa($keyword);
 	if(mysqli_num_rows($resultSubTbl)>0){
 		while($row = mysqli_fetch_array($resultSubTbl))
 		{
