798fc91dd62b6b02d56ecd345d06451d2291851a
Michael Liao <liaomichael3@gmail.com>
2017-12-06 13:44:47 -0600

    o

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -13,9 +13,9 @@ if (mysqli_connect_errno()) {
   //include 'filterCrimeData.php';
   //$data = json_decode(file_get_contents("php://input"));
   //$keyword = mysqli_real_escape_string($connect, $data->keyword);
-  mysqli_query($connect, "CREATE PROCEDURE `gadf`(IN @keyword) SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood=@keyword");
-  if (!$check1_res) {
-    printf("Error: %s\n", mysqli_error($con));
+  $rawr = mysqli_query($connect, "CREATE PROCEDURE `gadf`(IN @keyword) SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood=@keyword");
+  if (!$rawr) {
+    printf("Error: %s\n", mysqli_error($connect));
     exit();
 }
   // $stmt = $connection->prepare("SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
