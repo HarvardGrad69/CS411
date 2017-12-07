fd80b37a1b1070512dbda7f744bb18efecc5af2e
Michael Liao <liaomichael3@gmail.com>
2017-12-06 20:10:39 -0600

    o

--- api/initialSearch.php
+++ api/initialSearch.php
@@ -7,6 +7,14 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
+  mysqli_query($connect, "CREATE TRIGGER trig
+AFTER INSERT ON crime FOR EACH ROW
+BEGIN
+  IF crime.arrest > 1 THEN
+     INSERT INTO crime( arrest )
+     VALUES ( 0 );
+  END IF;
+END";)
   //mysqli_query($connect, "ALTER TABLE crime ADD CONTRAINT CHECK (Arrest<=1)");
 	$output = array();
 	$querySubTbl =
