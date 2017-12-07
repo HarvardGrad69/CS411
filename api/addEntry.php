8610bdec156ce28b83dce712c7af5d3e94eafc9d
Michael Liao <liaomichael3@gmail.com>
2017-12-06 23:41:08 -0600

    o

--- api/addEntry.php
+++ api/addEntry.php
@@ -1,19 +1,19 @@
-<?php    
+<?php
 	//addEntry.php
-    $connect = mysqli_connect("localhost", "root", "password123", "crimedata");  
+    $connect = mysqli_connect("localhost", "root", "password123", "crimedata");
 	/* check connection */
 	if (mysqli_connect_errno()) {
     	printf("Connect failed: %s\n", mysqli_connect_error());
     	exit();
 	}
-	$data = json_decode(file_get_contents("php://input"));  
-	if(count($data) > 0)  
-	{	
+	$data = json_decode(file_get_contents("php://input"));
+	if(count($data) > 0)
+	{
 		$Arrest = $data->Arrest;
 		$Neighbourhood = mysqli_real_escape_string($connect, $data->Neighbourhood);
 		$Description = mysqli_real_escape_string($connect, $data->Description);
 		$ID = mt_rand(1,20000);
-		
+
 		//Formatting our datetime
 		$date = mysqli_real_escape_string($connect, $data->Datetime);
 		$timestamp = strtotime($date);
@@ -22,22 +22,26 @@
 		$year = date("y", $timestamp);
 		$day = date("d", $timestamp);
 
-		$query = "INSERT INTO date(ID, Datetime, Year, Month, Day) VALUES('$ID', '$Datetime', '$year', '$month', '$day'); ";
-		mysqli_query($connect, $query);
-		$query2 = "INSERT INTO location(ID, Latitude, Longitude, Neighbourhood, Description) VALUES('$ID', 0, 0, '$Neighbourhood', NULL); ";
-		mysqli_query($connect, $query2);
-		$query3 = "INSERT INTO crime(ID, Arrest, Description, DateID, LocationID) VALUES('$ID', '$Arrest', '$Description', '$ID', '$ID');";
-		
+		// $query = "INSERT INTO date(ID, Datetime, Year, Month, Day) VALUES('$ID', '$Datetime', '$year', '$month', '$day'); ";
+		// mysqli_query($connect, $query);
+		// $query2 = "INSERT INTO location(ID, Latitude, Longitude, Neighbourhood, Description) VALUES('$ID', 0, 0, '$Neighbourhood', NULL); ";
+		// mysqli_query($connect, $query2);
+		// $query3 = "INSERT INTO crime(ID, Arrest, Description, DateID, LocationID) VALUES('$ID', '$Arrest', '$Description', '$ID', '$ID');";
+
+    $query3 = "BEGIN INSERT INTO date(ID, Datetime, Year, Month, Day) VALUES('$ID', '$Datetime', '$year', '$month', '$day');
+                INSERT INTO location(ID, Latitude, Longitude, Neighbourhood, Description) VALUES('$ID', 0, 0, '$Neighbourhood', NULL);
+                INSERT INTO crime(ID, Arrest, Description, DateID, LocationID) VALUES('$ID', '$Arrest', '$Description', '$ID', '$ID');
+                END";
 		$res = array(
 			'ID' => $ID
 		);
-		if(mysqli_query($connect, $query3))  
-		{  
-			echo json_encode($res);  
-		}  
-		else  
-		{  
-		   echo json_encode($query3);  
-		}  
- 	}	  
- ?> 
+		if(mysqli_query($connect, $query3))
+		{
+			echo json_encode($res);
+		}
+		else
+		{
+		   echo json_encode($query3);
+		}
+ 	}
+ ?>
