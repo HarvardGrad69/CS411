8ab0c37deb1b715e1a325d478519e0d12448d3e5
Michael Liao <liaomichael3@gmail.com>
2017-12-06 19:17:10 -0600

    o

--- api/initialSearch.php
+++ api/initialSearch.php
@@ -1,15 +1,15 @@
 <?php
 //initialSearch.php
 
-$connect = mysqli_connect("localhost", "root", "password123", "crimedata");  
+$connect = mysqli_connect("localhost", "root", "password123", "crimedata");
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-
+  mysqli_query($connect, "ALTER TABLE crime ADD CONTRAINT CHECK (Arrest<=1)");
 	$output = array();
-	$querySubTbl = 
+	$querySubTbl =
 		"SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID LIMIT 50";
 	$resultSubTbl = mysqli_query($connect, $querySubTbl);
 	if(mysqli_num_rows($resultSubTbl)>0){
