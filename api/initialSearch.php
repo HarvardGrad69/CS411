50b995b521be864dcfe16a3e34d7a3e147afa507
Michael Liao <liaomichael3@gmail.com>
2017-12-07 00:13:44 -0600

    o

--- api/initialSearch.php
+++ api/initialSearch.php
@@ -17,8 +17,8 @@ BEGIN
 END");
 mysqli_query($connect, "DROP INDEX on_neigh");
 mysqli_query($connect, "CREATE INDEX on_neigh ON location(Neighbourhood)");
-mysqli_query($connect, "ALTER TABLE crime DROP CONSTRAINT CHK_crime");
-mysqli_query($connect, "ALTER TABLE crime ADD CONSTRAINT CHK_crime CHECK (DATALENGTH(Description) > 0)");
+mysqli_query($connect, "ALTER TABLE date DROP CONSTRAINT CHK_crime");
+mysqli_query($connect, "ALTER TABLE date WITH CHECK ADD CONSTRAINT CHK_crime CHECK((DateTime<>N''))");
 mysqli_query($connect, "DROP PROCEDURE searchy");
 mysqli_query($connect, "CREATE PROCEDURE searchy @param1 VARCHAR(45) AS
                         BEGIN
@@ -28,7 +28,7 @@ mysqli_query($connect, "CREATE PROCEDURE searchy @param1 VARCHAR(45) AS
                         END;");
 	$output = array();
 	$querySubTbl =
-		"SELECT crime.ID, Arrest, crime.Description, date.Year, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID LIMIT 50";
+		"SELECT crime.ID, Arrest, crime.Description, crime.ID, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID LIMIT 50";
 	$resultSubTbl = mysqli_query($connect, $querySubTbl);
 	if(mysqli_num_rows($resultSubTbl)>0){
 		while($row = mysqli_fetch_array($resultSubTbl))
