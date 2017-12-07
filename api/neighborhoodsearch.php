47a8ac66f3265661f93d17a46670d2f013e77eb8
Michael Liao <liaomichael3@gmail.com>
2017-12-06 20:01:25 -0600

    o

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -13,7 +13,7 @@ if (mysqli_connect_errno()) {
   //include 'filterCrimeData.php';
   $data = json_decode(file_get_contents("php://input"));
   $keyword = mysqli_real_escape_string($connect, $data->keyword);
-  mysqli_query($connect, "CREATE PROCEDURE foofoo()
+  $res = mysqli_query($connect, "CREATE PROCEDURE foofoo()
                                 SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood
                                 FROM crime, location, date
                                 WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
@@ -27,7 +27,7 @@ if (mysqli_connect_errno()) {
   // $stmt = $connection->prepare("SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
   // $stmt->bind_param("sss", $keyword);
   //$stmt->execute();
-  $res = mysqli_query($connect,"call foofoo()");
+  //$res = mysqli_query($connect,"call foofoo()");
   //
   if(mysqli_num_rows($res)>0){
 		while($row = mysqli_fetch_array($res))
