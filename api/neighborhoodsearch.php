bef4f6bbee7b3f4a022f54b8faa21f5043ea8f81
Michael Liao <liaomichael3@gmail.com>
2017-12-07 20:06:42 -0600

    o

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -13,10 +13,10 @@ if (mysqli_connect_errno()) {
   //include 'filterCrimeData.php';
   $data = json_decode(file_get_contents("php://input"));
   $keyword = mysqli_real_escape_string($connect, $data->keyword);
-  $res = mysqli_query($connect, "
-                                SELECT crime.ID, Arrest, crime.Description, crime.ID, Neighbourhood
-                                FROM crime, location, date
-                                WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
+  // $res = mysqli_query($connect, "
+  //                               SELECT crime.ID, Arrest, crime.Description, crime.ID, Neighbourhood
+  //                               FROM crime, location, date
+  //                               WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
   //$doodoo = mysqli_query($connect, "CREATE VIEW poopoo AS SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='Bridgeport'");
   //$res = mysqli_query($connect, "SELECT * FROM poopoo");
   //mysqli_query($connect, "ALTER TABLE crime ADD CONTRAINT CHECK (Arrest<=1)");
@@ -27,7 +27,7 @@ if (mysqli_connect_errno()) {
   // $stmt = $connection->prepare("SELECT crime.ID, Arrest, crime.Description, Datetime, Neighbourhood FROM crime, location, date WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood='$keyword'");
   // $stmt->bind_param("sss", $keyword);
   //$stmt->execute();
-  //$res = mysqli_query($connect,"exec searchy @param1='$keyword' go;");
+  $res = mysqli_query($connect,"call searched($keyword)");
   //
   if(mysqli_num_rows($res)>0){
 		while($row = mysqli_fetch_array($res))
