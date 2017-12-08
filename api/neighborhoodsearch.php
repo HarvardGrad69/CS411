a37c18218bbf9ea957a4766c8a6fa321a862750a
Michael Liao <liaomichael3@gmail.com>
2017-12-07 21:45:11 -0600

    o

--- api/neighborhoodsearch.php
+++ api/neighborhoodsearch.php
@@ -32,21 +32,25 @@ if (mysqli_connect_errno()) {
   WHERE crime.LocationID = location.ID and crime.DateID = date.ID and location.neighbourhood=?")) {
   mysqli_stmt_bind_param($stmt, "s", $keyword);
   mysqli_stmt_execute($stmt);
+  $res = $stmt->get_result();
   //mysqli_store_result($stmt);
   //$res = mysqli_query($connect,"call searched($keyword)");
   //
   //echo $stmt;
-  $count = mysqli_stmt_num_rows($stmt);
-  if(mysqli_stmt_num_rows($stmt)>0) {
+  //$count = mysqli_stmt_num_rows($stmt);
+  //if((mysqli_num_rows($res)>0) {
   //for(;$count>0;count--){
-		while($row = mysqli_get_result($stmt))
-		{
-			$output[] = $row;
-		}
+		// while($row = mysqli_get_result($stmt))
+		// {
+		// 	$output[] = $row;
+		// }
     // Hi
+    while($result=$res->fetch_assoc()) {
+      $output[] = $result;
+    }
 		echo json_encode($output);
-	}
-  mysqli_stmt_close($stmt);
+	//}
+  //mysqli_stmt_close($stmt);
 }
 
 
